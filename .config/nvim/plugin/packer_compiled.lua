496dbe3e8a3a4f3fe7f0e705f58d5a209978b16e
Alfredo <alfredo.vargas-paredes@outlook.com>
2022-04-14 18:19:12 +0200

    new light colorscheme

--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -244,6 +244,11 @@ _G.packer_plugins = {
     path = "/home/alfredo/.local/share/nvim/site/pack/packer/start/toggleterm.nvim",
     url = "https://github.com/akinsho/toggleterm.nvim"
   },
+  ["tokyonight.nvim"] = {
+    loaded = true,
+    path = "/home/alfredo/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    url = "https://github.com/folke/tokyonight.nvim"
+  },
   ["vim-bbye"] = {
     loaded = true,
     path = "/home/alfredo/.local/share/nvim/site/pack/packer/start/vim-bbye",
