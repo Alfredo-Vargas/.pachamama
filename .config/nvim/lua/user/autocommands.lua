c01cda688757be0bee68c2ecb52d2689397390d1
Alfredo-Vargas <alfredo.vargas-paredes@outlook.com>
2022-05-22 11:18:56 +0200

    winbar settings

--- .config/nvim/lua/user/autocommands.lua
+++ .config/nvim/lua/user/autocommands.lua
@@ -1,81 +1,81 @@
--- -- vim.api.nvim_create_autocmd({ "User" }, {
--- --   pattern = { "AlphaReady" },
--- --   callback = function()
--- --     vim.cmd [[
--- --       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
--- --     ]]
--- -- })
---
--- vim.api.nvim_create_autocmd({ "FileType" }, {
---   pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+-- vim.api.nvim_create_autocmd({ "User" }, {
+--   pattern = { "AlphaReady" },
 --   callback = function()
 --     vim.cmd [[
---       nnoremap <silent> <buffer> q :close<CR> 
---       set nobuflisted 
+--       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+--     ]]
+-- })
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+  callback = function()
+    vim.cmd [[
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
+    ]]
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
+
+vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   callback = function()
+--     vim.cmd [[
+--       if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 --     ]]
 --   end,
 -- })
---
--- vim.api.nvim_create_autocmd({ "FileType" }, {
---   pattern = { "gitcommit", "markdown" },
---   callback = function()
---     vim.opt_local.wrap = true
---     vim.opt_local.spell = true
---   end,
--- })
---
--- vim.cmd "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
--- -- vim.api.nvim_create_autocmd({ "BufEnter" }, {
--- --   callback = function()
--- --     vim.cmd [[
--- --       if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
--- --     ]]
--- --   end,
--- -- })
---
--- vim.api.nvim_create_autocmd({ "VimResized" }, {
---   callback = function()
---     vim.cmd "tabdo wincmd ="
---   end,
--- })
---
--- vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
---   callback = function()
---     vim.cmd "quit"
---   end,
--- })
---
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd "tabdo wincmd ="
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+  callback = function()
+    vim.cmd "quit"
+  end,
+})
+
 vim.cmd [[
-  augroup _general_settings
-    autocmd!
-    autocmd FileType qf,help,man,lspinfo,spectre_panel nnoremap <silent> <buffer> q :close<CR>
-
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
-    autocmd CmdWinEnter * quit
-  augroup end
-
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
-
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd =
-  augroup end
-
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
+  " augroup _general_settings
+  "   autocmd!
+  "   autocmd FileType qf,help,man,lspinfo,spectre_panel nnoremap <silent> <buffer> q :close<CR>
+  "
+  "   autocmd BufWinEnter * :set formatoptions-=cro
+  "   autocmd FileType qf set nobuflisted
+  "   autocmd CmdWinEnter * quit
+  " augroup end
+  "
+  " augroup _git
+  "   autocmd!
+  "   autocmd FileType gitcommit setlocal wrap
+  "   autocmd FileType gitcommit setlocal spell
+  " augroup end
+  "
+  " augroup _markdown
+  "   autocmd!
+  "   autocmd FileType markdown setlocal wrap
+  "   autocmd FileType markdown setlocal spell
+  " augroup end
+  "
+  " augroup _auto_resize
+  "   autocmd!
+  "   autocmd VimResized * tabdo wincmd =
+  " augroup end
+  "
+  " augroup _alpha
+  "   autocmd!
+  "   autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
+  " augroup end
 
   augroup illuminate_augroup
     autocmd!
